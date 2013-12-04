67090745b7d905b13154349e856e130b2f0163b6
Alexandr <alexfree@meta.ua>
2013-12-04 21:13:33 +0200

    перемещены скрипты в футер и удалены ненужные

--- templates/store/footer.php
+++ templates/store/footer.php
@@ -1,2 +1,5 @@
+<script src="<?=TEMP::$js_dir?>/jquery.form.js"></script>
+<script src="<?=TEMP::$curr_temp_path?>/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?=TEMP::$curr_temp_path?>/datepicker/js/bootstrap-datepicker.js"></script>
 </body>
 </html>
--- templates/store/header.php
+++ templates/store/header.php
@@ -8,12 +8,8 @@
 	<link rel="stylesheet" href="<?=TEMP::$curr_temp_path?>/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?=TEMP::$curr_temp_path?>/datepicker/css/datepicker.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="<?=TEMP::$curr_temp_path?>/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 	<script src="<?=TEMP::$js_dir?>/jquery-1.9.1.min.js"></script>
 	<script src="<?=TEMP::$js_dir?>/main.js"></script>
-	<script src="<?=TEMP::$js_dir?>/jquery.form.js"></script>
-	<script src="<?=TEMP::$curr_temp_path?>/bootstrap/js/bootstrap.min.js"></script>
-	<script src="<?=TEMP::$curr_temp_path?>/datepicker/js/bootstrap-datepicker.js"></script>
 	<meta name="<?=TEMP::$title?>" content="application">
 	<!--[if lt IE 9]>
 		   <script>
